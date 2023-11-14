70b6ee8757bb5e2345b79c9d9d7e8c33d59906ae
MilagrosVi <135232221+MilagrosVi@users.noreply.github.com>
2023-11-14 11:15:12 -0300

    Update UserLogin.vue

--- frontend/src/components/UserLogin.vue
+++ frontend/src/components/UserLogin.vue
@@ -1,42 +1,90 @@
 <template>
-    <div>
-        <h1>Login</h1>
-        <form @submit="checkUser">
-            <input type="text" v-model="user.name" placeholder="User">
-            <input type="password" v-model="user.password" placeholder="Password">
-            <input type="submit" value="Login">
-        </form>
+    <div class="container  mt-4">
+        <div id="logInApp" class="col-sm-8 col-sm-offset-2">
+            <h1>{{nameApp}}</h1>
+            
+            <form v-on:submit="comprobarUsuario">
+                <input type="text" v-model="usuarioIngresado.nombreUsuario" class="form-control">
+                <input type="password" v-model="usuarioIngresado.contrasenia" class="form-control">
+                <input type="submit" value="Ingresar" class="btn btn-block btn-success">
+            </form>
+            <br>
+            
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: "UserLogin",
-    data () {
-        return {
-            user: {
-                name: "",
-                password: ""
-            }
-        }
-    },
-    methods: {
-        checkUser(e){
-            e.preventDefault();
+    data() {
+                return {
+                    nameApp: 'LogIn',
 
-            fetch('http://localhost:5000/api/login',{
-                method: "POST",
-                headers:{
-                    'Content-Type': 'application/json'
+                    usuarioIngresado: {
+                        nombreUsuario:'',
+                        contrasenia:''
+                    },
+                    usuarios: []
+                    /*usuarios: [
+                        {
+                            nombreUsuario: 'juan',
+                            contrasenia: 1234
+                        },
+                        {
+                            nombreUsuario: 'pablo',
+                            contrasenia: 2415
+                        },
+                        {
+                            nombreUsuario: 'ana',
+                            contrasenia: 7845
+                        }
+                    ]*/
+                }
+            },
+            methods: { //creamos un objetos con funciones (métodos)
+                getUsuarios(){
+                    fetch('http://10.32.0.38:5000/usuarios',{
+                        method:"GET",
+                        headers:{
+                            'Content-Type': 'application/json'
+                        }
+                    })
+                    .then(resp => resp.json())
+                    .then(data => {
+                    let usuarios = data.usuarios;
+                    console.log(usuarios);
+                    usuarios.forEach(usuario => {this.usuarios.push(usuario)})
+                    //this.articles.push(...data)
+                    })
+                    .catch(error =>{
+                        console.log(error);
+                    })
                 },
-                body: JSON.stringify(this.user)
+
+                comprobarUsuario: function(e){
+                    e.preventDefault();
+                    var flag = false;
+                    console.log(this.usuarioIngresado);
+                    this.usuarios.forEach(usuario => {
+                        if(this.usuarioIngresado.nombreUsuario == usuario.nombreUsuario && 
+                        this.usuarioIngresado.contrasenia == usuario.contrasenia){
+                            console.log('Acceso concedido');
+                            flag = true;
+                            window.location.replace("http://localhost:8080");
+                            return;
+                        }
+                    });
+
+                    if(flag==false)
+                        console.log('Acceso denegado');
+                },
+            },
+            created(){
+                this.getUsuarios();
             }
-            ).then( response => { console.log(response)})
-        }
-    }
 }
 </script>
 
-<styles>
+<style>
 
-</styles>
+</style>
