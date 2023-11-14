2bd277d948315ce6a6c387dfd2936eba648c40e1
MilagrosVi <135232221+MilagrosVi@users.noreply.github.com>
2023-11-14 11:26:25 -0300

    Update NavBar.vue

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -13,7 +13,7 @@
     <div class="right-nav"> 
         <ul class="ul-nav">
             <li class="nav-item1">
-            <router-link class="" aria-current="page" to="/UserLogin.vue">Log In</router-link>
+            <router-link class="" aria-current="page" to="/users">Log In</router-link>
             </li>
             <li class="nav-item2">
             <router-link class="" to="/select-days">Comprar Tickets</router-link>
