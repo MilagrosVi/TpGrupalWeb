4964c099031cea58b3aa0a065d2cc3ba75850de4
jbeloquiucema <134952934+jbeloquiucema@users.noreply.github.com>
2023-11-15 15:03:07 -0300

    Update NavBar.vue

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -6,7 +6,9 @@
 
     <div class="left-nav">
         <ul class="ul-nav">
-            <a href="/"> Home</a>
+            <li class="nav-item">
+                <router-link to="/">Home</router-link>
+            </li>
         </ul>
     </div>
 
@@ -38,39 +40,18 @@ export default {
     justify-content: space-between;
     align-items: center;
     padding: 1%;
-    background-color: rgb(154, 219, 228); /* Fondo de la barra */
+    background-color: black; /* Fondo de la barra */
 }
 
 .left-nav, .right-nav {
-    display: flex;
-    align-items: center;
-}
-
-.ul-nav {
-    display: flex;
-    list-style: none;
-    font-family: 'Manrope', sans-serif;
-    font-weight: 600;
-    font-size: small;
-}
-
-.ul-nav a {
-    text-decoration: none;
-    color: white; /* Color del texto en la barra */
-}
-
-.nav-item {
     padding: 10px;
     border-radius: 5px;
-    margin: 0 5px; /* Separación entre elementos */
-    transition: background-color 0.3s ease; /* Efecto de transición en el color de fondo */
+   
+
 }
 
-.nav-item:hover {
-    background-color: rgb(218, 205, 205); /* Color de fondo al pasar el ratón */
-}
+
+
 
 
 </style>
-
-
