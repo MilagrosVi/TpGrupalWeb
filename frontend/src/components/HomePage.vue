2f31acdf2812c4e109262945fe2d9fe8570d4ee0
jbeloquiucema <134952934+jbeloquiucema@users.noreply.github.com>
2023-11-15 15:05:10 -0300

    Update HomePage.vue

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -1,87 +1,78 @@
 <template>
-
   <section class="hero">
     <div class="hero-div">
       <h1>ROCK FEST</h1>
-      <h2 class="fechas"> 10 y 11  de Noviembre </h2>
-      <p class="lugar"> Hipodromo de San Isidro </p>
-   </div> 
+      <h2 class="fechas">10 y 11 de Noviembre</h2>
+      <p class="lugar">Hipódromo de San Isidro</p>
+    </div>
   </section>
- 
-
-
-
 </template>
 
 <script>
-// export default {
-
-//   data(){
-//     return {
-//       articles:[],
-//     }
-//   },
-//     methods:{
-//       getArticles(){
-//         fetch('http://localhost:5000/articles',{
-//           method:"GET",
-//           headers:{
-//             'Content-Type': 'application/json'
-//           }
-//         })
-//         .then(resp => resp.json())
-//         .then(data => {
-//           let articulos = data.articles;
-//           console.log(articulos);
-//           articulos.forEach(articulo => {this.articles.push(articulo)})
-//           //this.articles.push(...data)
-//         })
-//         .catch(error =>{
-//           console.log(error);
-//         })
-//       }
-//     },
-//     //este se llama una vez creado el objeto, ver lifecicle hooks
-//     created(){
-//       this.getArticles();
-//     }
-//   }
-
-
+export default {
+  data() {
+    return {
+      articles: [],
+    };
+  },
+  methods: {
+    getArticles() {
+      fetch('http://localhost:5000/articles', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((resp) => resp.json())
+        .then((data) => {
+          let articulos = data.articles;
+          console.log(articulos);
+          articulos.forEach((articulo) => {
+            this.articles.push(articulo);
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  created() {
+    this.getArticles();
+  },
+};
 </script>
 
 <style>
-
-.hero-div{
+.hero-div {
   overflow-x: hidden;
-  width:100vw;
+  width: 100vw;
   height: 50vh;
   background-image: url("../assets/Festival-1500x430-1.png");
   background-repeat: no-repeat;
-  background-size: cover ;
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
 }
 
-  .hero-div h1{
-    display: flex;
-    font-size: 3em;
-    justify-content:center;
-    color: rgb(154,219,228);
-  }
+.hero-div h1,
+.hero-div h2,
+.hero-div p {
+  font-family: Julius Sans One;
+  color: white;
+}
 
-  .fechas{display: flex;
+.hero-div h1 {
+  font-size: 10vh;
+}
+
+.fechas {
   font-size: 2em;
-  justify-content:center;
-    color: rgb(154,219,228);
+}
 
-  }
-
-  .lugar{
-  display: flex;
+.lugar {
   font-size: 1.5em;
-  justify-content:center;
-    color: rgb(154,219,228);
-
-  }
-  
-
+}
 </style>
